c563455fd00627d3e1ccaed25f2cf6d6fc6cd7f8
prin <printempw@gmail.com>
2022-01-14 06:03:59 +0800

    feat: use TypeORM and PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,8 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { UsersModule } from './users/users.module'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 const NODE_ENV = process.env.NODE_ENV ?? 'development'
 
@@ -17,6 +18,22 @@ const NODE_ENV = process.env.NODE_ENV ?? 'development'
         '.env.local',
         '.env'
       ]
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        autoLoadEntities: true,
+        logging: NODE_ENV === 'development' ? 'all' : undefined,
+        // ATTENTION: Database schema synchronization could cause data loss,
+        // don't use this option in production.
+        synchronize: NODE_ENV === 'development'
+      })
     }),
     AuthModule,
     UsersModule
